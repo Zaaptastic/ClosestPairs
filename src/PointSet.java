ecfdb08859bf2c21d3973a0829081b71dd19e645
Robert Chen <rbchen16@gmail.com>
2016-09-25 14:15:32 -0400

    Added test files

--- src/PointSet.java
+++ src/PointSet.java
@@ -1,4 +1,11 @@
+import java.awt.geom.Point2D;
+
 
 public class PointSet {
-
+	public static String closestPair(Point2D.Double[] points){
+		for (Point2D.Double p : points){
+			System.out.println(p);
+		}
+		return "done";
+	}
 }
